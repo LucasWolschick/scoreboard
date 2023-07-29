1d16d2516aaa3520da696bfffcd64f5dcf6af1b2
Lucas Wolschick <6264382+LucasWolschick@users.noreply.github.com>
2023-07-29 12:17:08 -0300

    muda formato da instrução

--- src/instrucao.h
+++ src/instrucao.h
@@ -1,34 +1,67 @@
 #ifndef INSTRUCAO
 #define INSTRUCAO
 
+#include <stdint.h>
+
 enum opcode_instrucao
 {
-    OP_ADD,
+    OP_ADD = 0,
     OP_ADDI,
     OP_SUB,
     OP_SUBI,
     OP_MUL,
     OP_DIV,
+
     OP_AND,
     OP_OR,
     OP_NOT,
+
     OP_BLT,
     OP_BGT,
     OP_BEQ,
     OP_BNE,
     OP_J,
+
     OP_LW,
-    OP_SW
+    OP_SW,
+
+    OP_EXIT,
 };
 typedef enum opcode_instrucao opcode_instrucao;
 
-typedef struct inst inst;
+struct inst_R
+{
+    unsigned int opcode : 6;
+    unsigned int rs : 5;
+    unsigned int rt : 5;
+    unsigned int rd : 5;
+    unsigned int extra : 11;
+};
+
+struct inst_I
+{
+    unsigned int opcode : 6;
+    unsigned int rt : 5;
+    unsigned int rs : 5;
+    unsigned int imm : 16;
+};
+
+struct inst_J
+{
+    unsigned int opcode : 6;
+    unsigned int address : 26;
+};
+
 struct inst
 {
-    opcode_instrucao opcode;
-    int op1;
-    int op2;
-    int op3;
+    union
+    {
+        uint32_t instrucao;
+        struct inst_R r;
+        struct inst_I i;
+        struct inst_J j;
+    };
 };
+typedef struct inst inst;
 
 #endif

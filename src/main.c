e4526358e12007c76978193768cc4bf48697c1cb
Guilherme-Clemente <guiclemente2003@hotmail.com>
2023-08-13 23:26:38 -0300

    Commitei caralho

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
-
+#define MIN_TAM_MEMO 100
 #include "parser.h"
 
 char *file_to_string(const char *path)
@@ -33,6 +33,51 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Uso: %s <caminho>\n", argv[0]);
         exit(1);
     }
+
+    if (argc  < 5)
+    {
+        fprintf(stderr, "Uso: %s -p <nome do programa> -m <tamanho da memória> [-o <nome do arquivo>]\n", argv[0]);
+        exit(1);
+    }
+
+    char *programFileName = NULL;
+    int memorySize = 0;
+    char *outputFileName = NULL;
+
+    for(int i = 1; i < argc; i++)
+    {
+        if(strcmp(argv[i], "-p") == 0)
+        {
+            programFileName = argv[i + 1];
+            i++;
+        }
+        else if (strcmp(argv[i], "-m") == 0)
+        {
+            memorySize = atoi(argv[i + 1]);
+            if(memorySize < MIN_TAM_MEMO)
+            {
+                fprintf(stderr, "Lucas Wolschick");
+                exit(1);
+            }
+            i++;
+        }
+        else if (strcmp(argv[i], "-o") == 0)
+        {
+            outputFileName = argv[i + 1];
+            i++;
+        }
+    }
+
+    if (programFileName == NULL || memorySize < MIN_TAM_MEMO)
+    {
+        fprintf(stderr, "ERRO");
+        exit(1);
+    }
+
+    // char *b = file_to_string(programFileName);
+    // int n_instrucoes;
+    // vector data, instrucoes;
+    // parse(b, &data, &instrucoes);
 
     char *b = file_to_string(argv[1]);
     int n_instrucoes;
